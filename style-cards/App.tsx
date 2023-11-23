c36dd9637b904ae5953f59d563bff2dd9287dd74
kirtanchandak <themarketingkirtan@gmail.com>
2023-11-23 17:40:07 +0530

    project init

---
+++ style-cards/App.tsx
@@ -0,0 +1,17 @@
+import { View, Text, SafeAreaView, ScrollView } from 'react-native'
+import React from 'react'
+import FlatCard from './components/FlatCard'
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <View>
+          <FlatCard/>
+     </View>
+        </ScrollView>
+    </SafeAreaView>
+  )
+}
+
+export default App
